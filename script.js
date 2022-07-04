34d50cc4cba0b3450f74259ed2e7ddbdd5d146c9
ChristopherRdeKoker <108460798+ChristopherRdeKoker@users.noreply.github.com>
2022-07-04 18:42:17 +0200

    OOP Intro

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const Person = function (firstName, Dob) {
+  this.firstName = firstName;
+  this.DateOfBirth = Dob;
+  const calcAge = function () {
+    2022 - this.DateOfBirth;
+  };
+};
+const leela = new Person('Leela', 3001);
+const fry = new Person('Fry', 1980);
+const bender = new Person('Bender', 2869);
+
+console.log(leela, fry, bender);
+//absolute limit of prototype, returns null. End of prototype scope/range
+console.log(leela.__proto__.__proto__.__proto__);
+console.log(`------Br-----`);
+console.log(Person.prototype);
