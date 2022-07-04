a63d9406cfd7ad18b3efbeb3aa45ec7308d07e8a
ChristopherRdeKoker <108460798+ChristopherRdeKoker@users.noreply.github.com>
2022-07-04 19:14:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,8 @@
 const Person = function (firstName, Dob) {
   this.firstName = firstName;
   this.DateOfBirth = Dob;
-  const calcAge = function () {
-    2022 - this.DateOfBirth;
-  };
 };
+
 const leela = new Person('Leela', 3001);
 const fry = new Person('Fry', 1980);
 const bender = new Person('Bender', 2869);
@@ -16,3 +14,22 @@ console.log(leela, fry, bender);
 console.log(leela.__proto__.__proto__.__proto__);
 console.log(`------Br-----`);
 console.log(Person.prototype);
+
+// console.log(fry.calcAge());
+
+Person.prototype.calcAge = function () {
+  console.log(`${Math.abs(2022 - this.DateOfBirth)}`);
+};
+
+leela.calcAge();
+bender.calcAge();
+
+Person.prototype.GreetMessageMr = function () {
+  console.log(`Good morning Mr.${this.firstName[0]}`);
+};
+Person.prototype.GreetMessageMs = function () {
+  console.log(`Good morning Ms.${this.firstName[0]}`);
+};
+
+leela.GreetMessageMs();
+fry.GreetMessageMr();
